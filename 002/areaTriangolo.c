2e44c4375e8bb90d3a4c996d2f8b6468293e6277
tommasosollo <114075798+tommasosollo@users.noreply.github.com>
2022-09-26 09:52:39 +0200

    areaTringolo
    aggiunta commenti

--- 002/areaTriangolo.c
+++ 002/areaTriangolo.c
@@ -1,17 +1,32 @@
+
+/*
+file: areaTriangolo.c
+autore: Tommaso Sollo
+classe: 3°H
+data: 26/09/2022
+
+calcolo area di un triangolo
+
+Input: base, altezza
+Elaborazione: Calcolo area triangolo
+Output: area
+*/
+
+
 #include <stdio.h>
 
 int main() 
 {
-  float base, altezza;
-  float area;
+  float base, altezza;  //input
+  float area; //output
   
-  printf("Inserisci il valore della base: ");
+  printf("Inserisci il valore della base: ");   //leggo valore base
   scanf("%f", &base);
   
-  printf("Inserisci il valore dell'altezza: ");
+  printf("Inserisci il valore dell'altezza: ");   //leggo valore altezza
   scanf("%f", &altezza);
   
-  area = base * altezza / 2;
+  area = base * altezza / 2;  //calcolo area
   
-  printf("area triangolo = %f", area);
+  printf("area triangolo = %f", area);  //stampa a schermo risultato
 }
