8c96f7f892ef96bf7ebfb3994ad3116ba506a63b
tommasosollo <114075798+tommasosollo@users.noreply.github.com>
2022-09-28 12:54:56 +0200

    Create media_numeri.c

---
+++ 003/media_numeri.c
@@ -0,0 +1,34 @@
+
+/*
+file: areaTriangolo.c
+autore: Tommaso Sollo
+classe: 3°H
+data: 28/09/2022
+calcolo media di tre numeri
+Input: num1, num2, num3
+Elaborazione: Media
+Output: media
+*/
+
+
+#include <stdio.h>
+
+int main() 
+{
+  float num1, num2, num3;  //input
+  float media; //output
+  
+  printf("Inserisci il primo numero: ");   //leggo valore num1
+  scanf("%f", &num1);
+  
+  printf("Inserisci il secondo numero: ");   //leggo valore num2
+  scanf("%f", &num2);
+  
+  printf("Inserisci il terzo numero: ");   //leggo valore num3
+  scanf("%f", &num3);
+  
+  
+  media = (num1 + num2 + num3)/3;  //calcolo media
+  
+  printf("media = %.2f", media);  //stampa a schermo risultato
+}
