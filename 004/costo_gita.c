159bc5ef3ff58d93d4318e5553cd588255cd4569
tommasosollo <114075798+tommasosollo@users.noreply.github.com>
2022-10-03 09:35:33 +0200

    Update costo_gita.c

--- 004/costo_gita.c
+++ 004/costo_gita.c
@@ -1,19 +1,31 @@
+/*
+file: costo_gita.c
+autore: Tommaso Sollo
+classe: 3°H
+data: 03/10/2022
+calcolo costo gita: 200€ al giorno di pullman, 40 € al giorno di albergo, 0.5€ al km 
+Input: giorni, km
+Elaborazione: pullman, albergo, euroalkm
+Output: costo
+*/
+
 #include <stdio.h>
 
 int main() {
-	int pullman=200, albergo=40, euroalkm=0.5;
 	
-	int giorni, km;
+	int giorni, km;		//input
 	
-	float costo;
+	int pullman=200, albergo=40, euroalkm=0.5;	//lavoro
 	
-	printf("Inserisci il numero di giorni: ");
+	float costo;	//output
+	
+	printf("Inserisci il numero di giorni: ");	//scanf dei giorni
 	scanf("%d", &giorni);
 	
-	printf("Inserisci numero di km: ");
+	printf("Inserisci numero di km: ");		//scanf dei km percorsi
 	scanf("%d", &km);
 	
-	costo = giorni*(pullman+albergo) + euroalkm * km;
+	costo = giorni*(pullman+albergo) + euroalkm * km;	// calcolo costo gita
 	
-	printf("Il costo della gita e' %.2f", costo);
+	printf("Il costo della gita e' %.2f", costo);		// printf del risultato
 }
