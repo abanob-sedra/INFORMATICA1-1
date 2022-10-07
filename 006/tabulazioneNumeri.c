f9ddd92ac1d2c63bcb79d83cd713a0c897a08083
tommasosollo <114075798+tommasosollo@users.noreply.github.com>
2022-10-07 13:16:46 +0200

    Create tabulazioneNumeri.c

---
+++ 006/tabulazioneNumeri.c
@@ -0,0 +1,27 @@
+/** ****************************************************************************************
+* \mainpage tabulazioneNumeri.c
+*
+* @brief Dato un numero inserito dall’utente, compreso tra 100 e 999, stampalo in modo che ogni cifra sia separata dalle altre da uno spazio.
+* Il progetto accetta ogni tipo di numero tra 100 e 999
+* Collaudato con numeri tra 100 e 999, inferiori a 100, superiori a 999
+* 
+* @author Tommaso Sollo
+* @date 07/10/2022
+*/
+
+
+#include <stdio.h>
+
+int main() {
+  int num;  //input
+  int n1, n2, n3; //output
+  
+  printf("Inserisci un numero tra 100 e 999: ");  //richiesta numero all'utente
+  scanf("%d", &num);
+  
+  n1 = num / 100;   //calcolo prima cifra
+  n2 = (num - n1 * 100) / 10;  //calcolo seconda cifra
+  n3 = (num - (n1*100 + n2*10));   //calcolo terza cifra
+  
+  printf("%d \t %d \t %d \t", n1, n2, n3);  //stampa a schermo
+  }
