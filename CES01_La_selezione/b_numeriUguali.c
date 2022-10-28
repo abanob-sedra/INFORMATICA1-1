b538fce2362808857b50ec629dfa0ac47aa0b6d6
tommasosollo <114075798+tommasosollo@users.noreply.github.com>
2022-10-28 10:52:08 +0200

    Update b_numeriUguali.c

--- CES01_La_selezione/b_numeriUguali.c
+++ CES01_La_selezione/b_numeriUguali.c
@@ -10,18 +10,20 @@
 #include <stdio.h>
 
 int main() {
-  int a,b;
-  printf("Inserisci due interi: ");
-  scanf("%d%d", &a, &b);
+  	int a,b;
+  	printf("Inserisci due interi: ");
+  	scanf("%d%d", &a, &b);
   
-  if(a==b) {
-    printf("i numeri sono uguali");
-  } else {
+  	if(a==b) {
+    	printf("i numeri sono uguali");
+  	} 
+  	else 
+  	{
     if(a>b){
       printf("%d>%d", a, b);
     } else {
       printf("%d>%d", b, a);
-  }
+  		}
  
+	}
 }
-
