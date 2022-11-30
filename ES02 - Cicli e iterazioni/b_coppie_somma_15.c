259176cf35da2e17a9a8e6c8cd83845d82c85e49
tommasosollo <tommaso.sollo2@gmail.com>
2022-11-30 12:20:16 +0000

    update

---
+++ ES02 - Cicli e iterazioni/b_coppie_somma_15.c
@@ -0,0 +1,17 @@
+/** ****************************************************************************************
+* \mainpage b_coppie_somma_15.c
+*
+* @brief Scrivere un programma che visualizzi tutte le coppie ordinate di numeri naturali la cui somma è 15. 
+* 
+* @author Tommaso Sollo
+* @date 30/11/2022
+*/
+
+#include <stdio.h>
+
+int main() 
+{
+	int n, i;
+	printf("Esercizio B Sollo Tommaso\n");
+
+}
