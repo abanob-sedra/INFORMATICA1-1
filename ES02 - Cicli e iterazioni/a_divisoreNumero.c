3033475637c5bafaa79357cefa5d8771dda3e96c
tommasosollo <114075798+tommasosollo@users.noreply.github.com>
2022-11-30 12:49:11 +0100

    Create a_divisoreNumero.c

---
+++ ES02 - Cicli e iterazioni/a_divisoreNumero.c
@@ -0,0 +1,25 @@
+/** ****************************************************************************************
+* \mainpage tabulazioneNumeri.c
+*
+* @brief Scrivere un programma che, richiesto un numero intero, visualizzi tutti i suoi divisori.
+* 
+* @author Tommaso Sollo
+* @date 30/11/2022
+*/
+
+#include <stdio.h>
+
+int main() 
+{
+	int n, i;
+	
+	printf("Inserisci un numero: ");
+	scanf("%d", &n);
+	
+	for(i = 2; i <= n/2; i++)
+	{
+		if(n%i==0)
+			printf("%d e' un divisore\n", i);
+	}
+	
+}
