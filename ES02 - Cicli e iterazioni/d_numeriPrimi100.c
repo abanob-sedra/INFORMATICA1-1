1e12d690fdc0f557133509c52d27aef1d6a04513
tommasosollo <tommaso.sollo2@gmail.com>
2022-12-02 20:21:35 +0000

    esercizio d

---
+++ ES02 - Cicli e iterazioni/d_numeriPrimi100.c
@@ -0,0 +1,38 @@
+/** ****************************************************************************************
+* \mainpage b_coppie_somma_15.c
+*
+* @brief Scrivere un programma che stampi tutti i numeri primi compresi tra 1 e 100. 
+*        Un numero è primo quando ha come divisore uno e sé stesso. Quindi è primo ciascun 
+*        numero naturale maggiore di 1 che sia divisibile solamente per 1 e per sé stesso.
+* 
+* @author Tommaso Sollo
+* @date 30/11/2022
+*/
+
+#include <stdio.h>      //inclusione librerie
+
+int main()  
+{
+    printf("\n--Esercizio D Sollo Tommaso--\n\n");
+
+    //diachiaro le variabili - c=variabile di controllo inizializzata a vero
+    int i1, i2, c=1;        
+
+    for(i1=2; i1<101; i1++) {       //itero tutti i numeri dal 2 al 100
+        
+        for(i2 = 2; i2<i1; i2++) {      //itero tutti i numeri dal 2 all'indice del primo ciclo
+        	
+            //se i1 è divisibile per un numero diverso da se stesso o 1 metto la variabile di controllo a falso
+            if(i1 % i2 == 0) 
+                c=0;          
+            
+        }
+
+        //stampo il numero solo se la varibile di controllo è su vero
+        if(c) 
+            printf("%d e' un numero primo \n", i1);
+        
+        //riassegno il valore vero alla variabile di controllo 
+        c=1;
+    }
+}
