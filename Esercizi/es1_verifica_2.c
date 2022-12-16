bf97c24dae91e3c8d332cbbd15fb12580b095c45
tommasosollo <tommaso.sollo2@gmail.com>
2022-12-16 12:40:58 +0000

    fd

---
+++ Esercizi/es1_verifica_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1, n2, somma = 0;
+
+    printf("Inserisci due numeri interi \n");
+    scanf("%d%d", &n1, &n2);
+
+    for(int i = n1; i<=n2; i++){
+        somma += i;
+
+        printf("%d\t", i);
+    }
+
+    printf("\nSomma = %d\n", somma);
+
+}
